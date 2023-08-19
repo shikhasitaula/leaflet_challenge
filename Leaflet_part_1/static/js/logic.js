257a839d98f584c6b9d982d83cef1f23b7233c78
shikhasitaula <129104368+shikhasitaula@users.noreply.github.com>
2023-08-19 14:38:42 -0700

    Added logic to js file.

--- Leaflet_part_1/static/js/logic.js
+++ Leaflet_part_1/static/js/logic.js
@@ -1,27 +1,33 @@
+// URL to fetch earthquake data from USGS GeoJSON feed
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// An array to store earthquake geometries
 geomeData = [];
 
+// Function to determine the color based on earthquake depth
 function getDepthColor(depth) {
     if (depth > 90) {
-        return '#d73027'; // Shallow earthquakes
+        return '#d73027'; // Red color (shallow)
     } else if (depth > 70) {
-        return '#fc8d59'; // Intermediate earthquakes
+        return '#fc8d59'; // Orange color 
     } else if (depth > 50) {
-        return '#fee08b'; // Intermediate earthquakes
+        return '#fee08b'; // Yellow color 
     } else if (depth > 30) {
-        return '#d9ef8b'; // Intermediate earthquakes
+        return '#d9ef8b'; // Light green color 
     } else if (depth > 10) {
-        return '#1a9850'; // Intermediate earthquakes
+        return '#1a9850'; // Dark green color 
     } else {
-        return 'lightgreen'; // Deep earthquakes
+        return 'lightgreen'; // Light green (deep)
     }
 }
 
-// Fetching and processing GeoJSON data
+// Fetching and processing GeoJSON data using D3.js
 d3.json(url).then(function (data) {
     console.log(data);
     const features = data.features;
     console.log(features);
+
+    // Extracting geometry information and populating geomeData array
 
     for (let index = 0; index < features.length; index++) {
         const geometry = features[index].geometry;
@@ -44,7 +50,7 @@ d3.json(url).then(function (data) {
         };
         const earthquakeMark = L.circleMarker([coordinates[1], coordinates[0]], circleMarker)
         .bindPopup(`<h3>Location:</h3> ${features[index].properties.place}<h3> Magnitude:</h3> ${magnitude}<h3> Depth:</h3> ${depth}`);
-
+        // Adding the marker to the array
     earthquakeMarkers.push(earthquakeMark);
 }
 
@@ -52,14 +58,14 @@ d3.json(url).then(function (data) {
     const earthquakeLayer = L.layerGroup(earthquakeMarkers);
     earthquakeLayer.addTo(myMap);
 
-    // Define the grades and corresponding colors
+    // setting up the colors for different depth levels.
     const grades = [-10, 10, 30, 50, 70, 90];
     const colors = ['#1a9850', '#d9ef8b', '#fee08b', '#fc8d59', '#d73027', 'lightgreen'];
 
     // Create a legend control
     const legend = L.control({ position: 'bottomright' });
 
-    // Function to generate the legend content
+    // Adding each depth range with its color.
     legend.onAdd = function (map) {
         const div = L.DomUtil.create('div', 'info legend');
         div.innerHTML = '<strong>Depth Legend</strong><br>';
@@ -76,12 +82,12 @@ d3.json(url).then(function (data) {
     // Add the legend to the map
     legend.addTo(myMap);
 });
-
+// Setting up our map with its center and zoom level.
 const myMap = L.map("map", {
     center: [37.0902, -95.7129],
     zoom: 5
 });
-
+// Adding the map's base layer using OpenStreetMap tiles.
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
