1c16dd00d1174baffb901cb2b7bf4fcd03abc6e7
shikhasitaula <129104368+shikhasitaula@users.noreply.github.com>
2023-08-19 14:21:09 -0700

    Added logic to the css, js file and updated readme.

--- Leaflet_part_1/static/js/logic.js
+++ Leaflet_part_1/static/js/logic.js
@@ -1,55 +1,87 @@
-url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-geometryData = [];
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+geomeData = [];
 
 function getDepthColor(depth) {
-    if (depth < 50) {
-      return 'green'; // Shallow earthquakes
-    } else if (depth < 150) {
-      return 'orange'; // Intermediate earthquakes
+    if (depth > 90) {
+        return '#d73027'; // Shallow earthquakes
+    } else if (depth > 70) {
+        return '#fc8d59'; // Intermediate earthquakes
+    } else if (depth > 50) {
+        return '#fee08b'; // Intermediate earthquakes
+    } else if (depth > 30) {
+        return '#d9ef8b'; // Intermediate earthquakes
+    } else if (depth > 10) {
+        return '#1a9850'; // Intermediate earthquakes
     } else {
-      return 'red'; // Deep earthquakes
+        return 'lightgreen'; // Deep earthquakes
     }
-  }
+}
+
 // Fetching and processing GeoJSON data
-d3.json(url).then(function(data){
+d3.json(url).then(function (data) {
     console.log(data);
     const features = data.features;
     console.log(features);
-  
+
     for (let index = 0; index < features.length; index++) {
-      const geometry = features[index].geometry;
-      geometryData.push(geometry);
+        const geometry = features[index].geometry;
+        geomeData.push(geometry);
     }
-  
-    console.log(geometryData);
-  
+
+    console.log(geomeData);
+
     // Create circular markers and populate earthquakeMarkers array
     const earthquakeMarkers = [];
-    for (let index = 0; index < geometryData.length; index++) {
-      const coordinates = geometryData[index].coordinates;
-      const magnitude = features[index].properties.mag; // Get magnitude
-      const depth = coordinates[2]; // Get depth
-  
-      const circleOptions = {
-        radius: magnitude * 3, // Customize the radius based on magnitude
-        color: getDepthColor(depth), // Use the depth color function from your original code
-        fillOpacity: 0.7
-      };
-  
-      const earthquakeMark = L.circleMarker([coordinates[1], coordinates[0]], circleOptions); // Note the order of lat and long
-      earthquakeMarkers.push(earthquakeMark);
-    }
-  
+    for (let index = 0; index < geomeData.length; index++) {
+        const coordinates = geomeData[index].coordinates;
+        const magnitude = features[index].properties.mag; // Get magnitude
+        const depth = coordinates[2]; // Get depth
+
+        const circleMarker = {
+            radius: magnitude * 3, // Customize the radius based on magnitude
+            color: getDepthColor(depth), // Use the depth color function from your original code
+            fillOpacity: 1
+        };
+        const earthquakeMark = L.circleMarker([coordinates[1], coordinates[0]], circleMarker)
+        .bindPopup(`<h3>Location:</h3> ${features[index].properties.place}<h3> Magnitude:</h3> ${magnitude}<h3> Depth:</h3> ${depth}`);
+
+    earthquakeMarkers.push(earthquakeMark);
+}
+
     // Create a layer group and add it to the map
     const earthquakeLayer = L.layerGroup(earthquakeMarkers);
     earthquakeLayer.addTo(myMap);
-  });
-  
-  const myMap = L.map("map", {
+
+    // Define the grades and corresponding colors
+    const grades = [-10, 10, 30, 50, 70, 90];
+    const colors = ['#1a9850', '#d9ef8b', '#fee08b', '#fc8d59', '#d73027', 'lightgreen'];
+
+    // Create a legend control
+    const legend = L.control({ position: 'bottomright' });
+
+    // Function to generate the legend content
+    legend.onAdd = function (map) {
+        const div = L.DomUtil.create('div', 'info legend');
+        div.innerHTML = '<strong>Depth Legend</strong><br>';
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + colors[i] + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + (grades[i + 1] - 1) + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    // Add the legend to the map
+    legend.addTo(myMap);
+});
+
+const myMap = L.map("map", {
     center: [37.0902, -95.7129],
     zoom: 5
-  });
-  
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-# Overview
+
+### Earthquake Data Visualization
 ### Leaflet Challenge
 
 The United States Geological Survey, or USGS for short, is responsible for providing scientific data about natural hazards, the health of our ecosystems and environment, and the impacts of climate and land-use change. Their scientists develop new methods and tools to supply timely, relevant, and useful information about the Earth and its processes.
 
 The USGS is interested in building a new set of tools that will allow them to visualize their earthquake data. They collect a massive amount of data from all over the world each day, but they lack a meaningful way of displaying it. In this challenge, you have been tasked with developing a way to visualize USGS data that will allow them to better educate the public and other government organizations (and hopefully secure more funding) on issues facing our planet.
 
-## Features
+## What It Can Do
 
-- Fetches earthquake data from the USGS GeoJSON feed.
-- Creates circular markers on the map to represent earthquakes.
-- Marker size is determined by the magnitude of the earthquake.
-- Marker color is determined by the depth of the earthquake.
-- Clicking on a marker displays a popup with earthquake details.
+- **Get Earthquake Info:** I fetch data from USGS to find out where earthquakes are and how strong they are.
+  
+- **Markers:** I create markers on the map to show where earthquakes happened. The bigger the marker, the bigger the earthquake.
+  
+- **Colors for Depth:** I use colors to show how deep the earthquake is. Deeper earthquakes have different colors.
+  
+- **bindPpos:** When you click on a marker, it tells you where the earthquake happened, how strong it was, and how deep it went.
 
-## Basis:
 
-First I created a repository and cloned it to my local machine. Then I opended the "index.html" file in a web browser to view the interactive map. I imported the dependencies like leaflet and D3.js for the data visualizations. 
+## How I Get started:
+
+First, I created a repository and cloned it to my local machine. Then, I opended the "index.html" file in a web browser to view the interactive map. I imported the dependencies like leaflet and D3.js for the data visualizations. 
+
+The logic above code fetches earthquake data, processes it, creates circular markers with varying sizes and colors based on earthquake magnitude and depth, and displays these markers on a Leaflet map.
+
+## code: 
+1. **Data extraction:** I used D3.js to get earthquake data from USGS and make sense of it.
+
+2. **Markers That Talk:** Each earthquake is a dot on the map. The bigger the dot, the bigger the quake!
+
+3. **Color Secrets:** I used different colors for different depths of earthquakes. It helps you see which ones are deeper.
+
+4. **Legend Guide:** I made a small box that tells you which color means what depth. So you're never confused!
+
+5. **Map Wizardry:** I used Leaflet to make a map. It's like a big canvas where we put our earthquake markers.
